9c4113a37295f8bce80ff4fb14f27d6eba34081d
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-03-07 21:17:53 +0530

    RMX1971: Build custom light hal
    * Based on oneplus sm8150 and rc has been stripped down of graphic group
    * Change max brightness node value according to RMX1971's dts
    
    Change-Id: Iac6309959cd10e1baf79494d2a9e134143ae3671

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.realme
+
 # Media
 PRODUCT_PACKAGES += \
     libstagefright_omx \
