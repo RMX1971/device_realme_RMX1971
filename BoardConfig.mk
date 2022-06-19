RMX1971/archived_device_realme_RMX1971

RMX1971: Allow double-tap to wake
Change-Id: I457dce0387db8138b33515f4c0d6023aa5cba5c2

BoardConfig.mk
@@ -48,6 +48,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 
 # Display
 TARGET_SCREEN_DENSITY := 480
+TARGET_TAP_TO_WAKE_NODE := /proc/touchpanel/double_tap_enable
 
 # Extra images
 TARGET_NO_BOOTLOADER := true

RMX1971/device_realme_RMX1971

BoardConfig.mk
@@ -47,6 +47,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 
 # Display
 TARGET_SCREEN_DENSITY := 480
+TARGET_TAP_TO_WAKE_NODE := /proc/touchpanel/DouTap_enable
 
 # Extra images
 TARGET_NO_BOOTLOADER := true